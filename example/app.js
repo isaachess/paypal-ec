100711b5906ea1e6a208336aeb1979a0753ca5bc
mason <mason@dreamerslab.com>
2012-06-18 19:33:21 +0800

    add example

---
+++ example/app.js
@@ -0,0 +1,96 @@
+var PayPalEC = require( '../lib/paypal-ec' );
+var express  = require( 'express' );
+var util     = require( 'util' );
+
+var app = express.createServer();
+
+app.configure( function (){
+  app.set( 'views', __dirname + '/views' );
+  app.set( 'view engine', 'ejs' );
+  app.use( express.methodOverride());
+  app.use( express.bodyParser());
+  app.use( app.router );
+  app.use( express.errorHandler({
+    dumpExceptions : true,
+    showStack      : true
+  }));
+});
+
+app.helpers({
+  inspect : util.inspect
+});
+
+var cred = {
+  username  : 'seller_1339472528_biz_api1.gmail.com',
+  password  : '1339472553',
+  signature : 'AFcWxV21C7fd0v3bYYYRCpSSRl31Af-aECo8vsiP1HospgIyBCFncbx3'
+};
+
+var opts = {
+  sandbox : true,
+  version : '78.0'
+}
+
+app.get( '/checkout', function ( req, res, next ){
+  var ec     = new PayPalEC( cred, opts );
+  var params = {
+    returnUrl : 'http://localhost:3000/confirm',
+    cancelUrl : 'http://localhost:3000/cancel',
+    SOLUTIONTYPE                   : 'sole',
+    PAYMENTREQUEST_0_AMT           : '10.0',
+    PAYMENTREQUEST_0_DESC          : 'Something',
+    PAYMENTREQUEST_0_CURRENCYCODE  : 'USD',
+    PAYMENTREQUEST_0_PAYMENTACTION : 'Sale',
+  };
+
+  ec.set( params, function ( err, data ){
+    if( err ) return next( err );
+
+    res.render( 'checkout', {
+      title : 'Payment Checkout',
+      data  : data
+    });
+  });
+});
+
+app.get( '/confirm', function ( req, res, next ){
+  var ec     = new PayPalEC( cred, opts );
+  var params = {
+    TOKEN : req.query.token
+  };
+
+  ec.get_details( params, function ( err, data ){
+    if( err ) return next( err );
+
+    res.render( 'confirm', {
+      title : 'Payment Confirmation',
+      data  : data
+    });
+  });
+});
+
+app.get( '/complete', function ( req, res, next ){
+  var ec     = new PayPalEC( cred, opts );
+  var params = {
+    TOKEN   : req.query.token,
+    PAYERID : req.query.PayerId
+  };
+
+  ec.get_details( params, function ( err, data ){
+    if( err ) return next( err );
+
+    ec.do_payment( data, function ( err, data ){
+      if( err ) return next( err );
+
+      res.render( 'complete', { title : 'Payment Completed' });
+    });
+  });
+});
+
+app.get( '/cancel', function ( req, res, next ){
+  res.render( 'cancel', { title : 'Payment Cancelled' });
+});
+
+app.listen( 3000 );
+
+
