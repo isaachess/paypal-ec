8cc404c0f90dde53535a15e0bd6f51cf5d85ec15
mason <mason@dreamerslab.com>
2012-07-25 00:02:15 +0800

    update example to include recurring payment

--- example/app.js
+++ example/app.js
@@ -31,68 +31,87 @@ var opts = {
   version : '88.0'
 };
 
-var item = {
-  returnUrl : 'http://localhost:3000/confirm',
-  cancelUrl : 'http://localhost:3000/cancel',
-  SOLUTIONTYPE                   : 'sole',
-  PAYMENTREQUEST_0_AMT           : '10.0',
-  PAYMENTREQUEST_0_DESC          : 'Something',
-  PAYMENTREQUEST_0_CURRENCYCODE  : 'USD',
-  PAYMENTREQUEST_0_PAYMENTACTION : 'Sale'
+var plans = {
+  normal : {
+    returnUrl : 'http://localhost:3000/confirm?plan=normal',
+    cancelUrl : 'http://localhost:3000/cancel',
+    SOLUTIONTYPE                   : 'sole',
+    PAYMENTREQUEST_0_AMT           : '10.00',
+    PAYMENTREQUEST_0_DESC          : 'One-time Purchase',
+    PAYMENTREQUEST_0_CURRENCYCODE  : 'USD',
+    PAYMENTREQUEST_0_PAYMENTACTION : 'Sale'
+  },
+
+  recurring : {
+    returnUrl : 'http://localhost:3000/confirm?plan=recurring',
+    cancelUrl : 'http://localhost:3000/cancel',
+    DESC                           : 'Recurring Payment',
+    AMT                            : '0.99',
+    CURRENCYCODE                   : 'USD',
+    PROFILESTARTDATE               : '2012-08-01T03:00:00',
+    L_BILLINGTYPE0                 : 'RecurringPayments',
+    L_BILLINGAGREEMENTDESCRIPTION0 : 'Recurring Payment',
+    BILLINGPERIOD                  : 'Month',
+    BILLINGFREQUENCY               : '1'
+  }
 };
 
 var ec = new PayPalEC( cred, opts );
 
-app.get( '/checkout', function ( req, res, next ){
-  var params = item;
+app.get( '/plans', function ( req, res, next ){
+  res.render( 'plans' );
+});
+
+app.post( '/checkout', function ( req, res, next ){
+  var params = plans[ req.body.plan ];
 
   ec.set( params, function ( err, data ){
     if( err ) return next( err );
 
-    res.render( 'checkout', {
-      title : 'Payment Checkout',
-      data  : data
-    });
+    res.redirect( data.PAYMENTURL );
   });
 });
 
 app.get( '/confirm', function ( req, res, next ){
-  var params = {
-    TOKEN : req.query.token
-  };
+  var plan   = req.query.plan;
+  var params = plans[ plan ];
 
-  ec.get_details( params, function ( err, data ){
+  params.TOKEN   = req.query.token;
+  params.PAYERID = req.query.PayerID;
+
+  if( plan == 'normal' ){
+    ec.do_payment( params, function ( err, data ){
+      if( err ) return next( err );
+
+      res.redirect( '/status?token=' + params.TOKEN );
+    });
+  }else{
+    ec.create_rpp( params, function ( err, data ){
+      if( err ) return next( err );
+
+      res.redirect( '/status?token=' + params.TOKEN );
+    });
+  }
+});
+
+app.get( '/cancel', function ( req, res, next ){
+  res.redirect( '/status?token=' + req.query.token );
+});
+
+app.get( '/status', function ( req, res, next ){
+  var token = req.query.token;
+
+  ec.get_details({ token : token }, function ( err, data ){
     if( err ) return next( err );
 
-    res.render( 'confirm', {
-      title : 'Payment Confirmation',
+    res.render( 'status', {
+      title : 'Payment Status',
       data  : data
     });
   });
 });
 
-app.get( '/complete', function ( req, res, next ){
-  var params = item;
-
-  params.TOKEN   = req.query.token;
-  params.PAYERID = req.query.PayerID;
-
-  ec.do_payment( params, function ( err, data ){
-    if( err ) return next( err );
-
-    res.render( 'complete', {
-      title : 'Payment Completed'
-    });
-  });
-});
-
-app.get( '/cancel', function ( req, res, next ){
-  res.render( 'cancel', {
-    title : 'Payment Cancelled'
-  });
-});
-
-console.log( 'Server started on http://localhost:3000' );
+console.log( '\nServer started on http://localhost:3000\n' );
 app.listen( 3000 );
 
 
